6cc4a14f04ab34d185ca6b33d333b4cd2f2333ce
Kristiyan Dzhurenov <dzurenovkris@abv.bg>
2025-10-19 21:02:57 +0300

    Made .gitignore file

--- HRManagementSystem/obj/Debug/net9.0/HRManagementSystem.AssemblyInfo.cs
+++ HRManagementSystem/obj/Debug/net9.0/HRManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0e1a171faa6e418ff811858b5a98d67510840e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
